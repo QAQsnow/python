d657c0529437b2213a2e4a45c22b966aa69d98dc
snow <luchuntian1314520@163.com>
2017-08-16 15:51:07 +0800

    hello.py and add do_for.py

---
+++ do_for.py
@@ -0,0 +1,6 @@
+names=['snow','wat','git'];
+for name in names:
+	print(name)
+	
+for num in range(10):
+	print(num)
--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 name=input('please enter your name：')
 print('hello ',name);
 print(1024*768);
+names=['snow','dragon','xiaomi'];
+for name in names:
+	print(name);
